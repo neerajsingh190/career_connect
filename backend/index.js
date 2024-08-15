bf2d905dea76e0de3f2603c13a050ab6a6fcd50c
neerajsingh190 <112683517+neerajsingh190@users.noreply.github.com>
2024-08-15 21:51:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://career-connect-1.onrender.com/',
+    origin:'https://career-connect-1.onrender.com',
     credentials:true
 }
 
